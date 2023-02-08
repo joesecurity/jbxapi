47dcebc1922e198018000b29de610b2509020424
abulhol <benjamin.gathmann@gmail.com>
2023-02-08 11:31:06 +0100

    Capture command line

--- jbxapi.py
+++ jbxapi.py
@@ -1105,6 +1105,9 @@ def cli(argv):
     def joelab_images_list(joe, args):
         print_json(joe.joelab_images_list(args.machine))
 
+    def joelab_images_capture(joe, args):
+        print_json(joe.joelab_images_capture(args.machine, args.image))        
+
     def joelab_images_reset(joe, args):
         print_json(joe.joelab_images_reset(args.machine, args.image))
 
@@ -1467,6 +1470,13 @@ def cli(argv):
             help="List the stored images.")
     joelab_images_list_parser.add_argument("--machine", required=True, help="Joe Lab machine ID")
     joelab_images_list_parser.set_defaults(func=joelab_images_list)
+
+    # joelab images capture
+    joelab_images_reset_parser = joelab_images_subparsers.add_parser('capture', parents=[common_parser],
+            help="Capture a machine image")
+    joelab_images_reset_parser.add_argument("--machine", required=True, help="Joe Lab machine ID")
+    joelab_images_reset_parser.add_argument("--image", help="Image ID")
+    joelab_images_reset_parser.set_defaults(func=joelab_images_capture)
 
     # joelab images reset
     joelab_images_reset_parser = joelab_images_subparsers.add_parser('reset', parents=[common_parser],
